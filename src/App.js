7e0e18df5ca0006dde054905212d5eafe4ebe81a
Hajaniaina Andrianavalona <hajaniaina.andri@gmail.com>
2022-01-31 19:08:38 +0300

    [code]: CLEANing the App code, removes useless variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import LoadingBar from 'react-redux-loading'
 import { styled } from '@mui/material/styles'
-import { Container, Box, Grid, Paper, Avatar, Divider, Stack, Chip
-  } from '@mui/material'
-import { Routes, Route, useParams } from 'react-router-dom'
-import NavTabs from './components/muis/NavTabs'
+import { Container, Box } from '@mui/material'
 import MiniDrawer from './components/muis/MiniDrawer'
-import Home from './components/muis/Home'
 
-class App extends Component {
-  render () {
-    return (
-      <Container maxWidth='xxl' sx={{ bgcolor: 'white', height:'50%', textAlign: 'center', color:'white' }} >
-        <Box sx={{height:'60px'}} />
-        <MiniDrawer />
-      </Container>
-    );
-  }
+// Styles
+const style = { 
+  bgcolor: 'white', 
+  height:'50%', 
+  textAlign: 'center', 
+  color:'white' 
+}
+
+function App () {
+  return (
+    <Container maxWidth='xxl' sx={style} >
+      <Box sx={{height:'60px'}} />
+      <MiniDrawer />
+    </Container>
+  );
 }
 
 export default App;
