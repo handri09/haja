ad4b71962ee3f5762ff4047466c220ebf387159e
Hajaniaina Andrianavalona <hajaniaina.andri@gmail.com>
2022-02-02 15:05:20 +0300

    [code]: UPDATED the profil text to be more readable

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,4 +1,4 @@
-export const presentation = `
+const presentation1 = `
 	Fort de mes 4 années d’expérience dans une entreprise minière renommée à Madagascar, 
 l'environnement industriel n'a plus de secret pour moi. 
 J’ai intégré le projet Ambatovy depuis 2017 en tant que Technicien du système de contrôle. 
@@ -11,4 +11,19 @@ les pannes ont été détectées très rapidement à l’aide des notifications 
 Passionné de nouvelles technologies, 
 j’aime mon travail et je suis très curieux en ce qui concerne les nouvelles découvertes professionnelles. 
 Mes principaux loisirs sont le football, la musculation et la musique.
+`
+
+export const presentation = `
+👋 Hi, I' Haja
+💞️ I have 4 years of experience as OSISoft PI Administrator and Developer. In my current role, I use my developer skills and knowledge in developping UI for real-time data entry and data monitoring (using Python, HTML/CSS, JavaScript and ReactJS).
+👀 I am interested in React Developer position. 
+🌱 I'm currently improving my ReactJS skills, as well as learning Algo & Data Structure... 
+🌱 I earn a certificate as React Developer by Udacity in October 2022 https://graduation.udacity.com/confirm/DDKJTMUA
+🌱 Earn as well a certificate as Data Scientis in January 2022 https://graduation.udacity.com/confirm/3LJ5GUKD
+💞️ I'm looking to collaborate on an interesting React Project and an Open Source Project.
+📫 Please reach me at hajaniaina.andri@gmail.com +261 34 27 536 51
+
+...My greatest achievement was the downtime management system developed with my team in JavaScript and ASP.NET. 
+Concerned about results, I focus on achieving the set objectives and continuous improvements. 
+Passionate about new technologies and always interested in new discoveries, I consider changes as challenges.
 `
